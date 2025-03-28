032704f7e3160d3235e5b779d27561e851519c77
Siddhartha Maity <siddharthamaity1993@gmail.com>
2025-03-29 03:26:30 +0530

    chore: Update feature description for Dockerfile to clarify Node.js and Bun versions

--- src/app/(delete-this-and-modify-page.tsx)/SetupDetails.tsx
+++ src/app/(delete-this-and-modify-page.tsx)/SetupDetails.tsx
@@ -32,7 +32,7 @@ const FEATURES: Feature[] = [
     { name: 'App Directory' },
     { name: 'System, Light & Dark Mode' },
     { name: 'Next Bundle Analyzer' },
-    { name: 'Dockerfile using Node.js 22.14.0 & Bun 1.2.7 (alpine)' }
+    { name: 'Dockerfile for Node.js 22.14.0 & Bun 1.2.7 (alpine)' }
 ];
 
 const ESLINT_PLUGINS: Plugin[] = [
